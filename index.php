4565d6a83e02006ab622d8bff4d93ce5df3a15fd
qwertyuiopngsdfg <pngsdfg@outlook.jp>
2018-05-26 11:47:38 +0000

    style for first view

--- index.php
+++ index.php
@@ -2,11 +2,20 @@
 <html lang="ja">
     <head>
         <meta charset="utf-8">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/styles.css">
         <title>一人暮らしシミュレーター</title>
     </head>
     <body>
-        <h1>一人暮らしシミュレーター</h1>
-        <a href="">初期費用を計算する</a>
-        <a href="">毎月の生活費を計算する。</a>
+        <header>
+            <h1>一人暮らしシミュレーター</h1>
+        </header>
+        <main>
+            <a href="">初期費用を計算する</a>
+            <a href="">毎月の生活費を計算する</a>
+        </main>
+        <footer>
+            <p>© 2018 qwertyuiopngsdfg</p>
+        </footer>
     </body>
 </html>
