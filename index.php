67e6bcb50f6243df28c2c04a87f16cf945c3efba
qwertyuiopngsdfg <pngsdfg@outlook.jp>
2018-05-26 09:42:20 +0000

    create top view

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>一人暮らしシミュレーター</title>
+    </head>
+    <body>
+        <h1>一人暮らしシミュレーター</h1>
+        <a href="">初期費用を計算する</a>
+        <a href="">毎月の生活費を計算する。</a>
+    </body>
+</html>
