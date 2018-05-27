cac942c8ac19e4ed71e7c7f3c120225a2a1339cd
qwertyuiopngsdfg <pngsdfg@outlook.jp>
2018-05-27 12:25:09 +0000

    create initial_cost.php

--- index.php
+++ index.php
@@ -7,15 +7,9 @@
         <title>一人暮らしシミュレーター</title>
     </head>
     <body>
-        <header>
-            <h1>一人暮らしシミュレーター</h1>
-        </header>
-        <main>
-            <a href="">初期費用を計算する</a>
-            <a href="">毎月の生活費を計算する</a>
-        </main>
-        <footer>
-            <p>© 2018 qwertyuiopngsdfg</p>
-        </footer>
+        <h1>一人暮らしシミュレーター</h1>
+        <a href="initial_cost.php">初期費用を計算する</a>
+        <a href="running_cost.php">毎月の生活費を計算する</a>
+        <p>© 2018 qwertyuiopngsdfg</p>
     </body>
 </html>
